e97d45e04805967cc5abdcbf7df4a128908d59d4
Daniel Christenson <55254935+Danielson9712@users.noreply.github.com>
2021-05-11 16:17:01 -0400

    initialize firebase, components

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,25 @@
+import firebase from 'firebase';
 
 function App() {
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyC-tTDGr6lbMP9klqQdC8uOXym4B8mJzbo",
+    authDomain: "minecraft-website-a445e.firebaseapp.com",
+    projectId: "minecraft-website-a445e",
+    storageBucket: "minecraft-website-a445e.appspot.com",
+    messagingSenderId: "989798867747",
+    appId: "1:989798867747:web:5c22f9d93b47685c1ae296",
+    measurementId: "G-0HBL1RLW5T"
+  };
+
+  if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+  } else {
+    firebase.app(); // if already initialized, use that one
+
+  }
+
+
   return (
     <div className="App">
       <h1> Welcome back to DissidiaCraft! </h1>
